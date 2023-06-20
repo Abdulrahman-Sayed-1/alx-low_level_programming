e3317c85cd860c222672658c0f97e795b7084e98
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-06-20 09:02:42 -0700

    1

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ *
+ * File: main.h
+ * 
+ * Description: a header file that contains all the declarations for 
+ * all functions used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+
+#endif
