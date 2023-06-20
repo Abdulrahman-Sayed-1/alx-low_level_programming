f633da61235526a7948462b0ccc462b13adee553
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-06-20 10:40:37 -0700

    1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints letters 10 times
+ * in lowecase followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int cnt = 10;
+	char c = 'a';
+
+	while (cnt--)
+	{
+		for (; c <= 'z'; c++)
+			_putchar((char)c);
+	}
+
+	_putchar('\n');
+}
