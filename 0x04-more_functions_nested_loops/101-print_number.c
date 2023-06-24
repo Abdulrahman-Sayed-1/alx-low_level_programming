1fb45cf2ad7ba9645259babc2f9638af3fe7e8ad
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-06-23 19:12:49 -0700

    1

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number using only _putchar function
+ * @n: The Number To Be Printed
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
