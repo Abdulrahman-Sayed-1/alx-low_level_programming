1982248517bdbaa366e3b9f4f5f6dc8025f36a2c
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-02 06:25:16 -0700

    0-puts_recursion.c

---
+++ 0x08-Recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _puts_recursion - prints a string's characters.
+ *
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_putchar(*s);
+
+	_puts_recursion(++*s);
+}
