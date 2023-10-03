0dae1b4b00db1de31800fc910ba95198d10330ad
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-03 14:49:15 -0700

    1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * sqrt - calculates the square root of a number greater than zero.
+ *
+ * @n: The number whose square root will be calculated.
+ * @guess: A helper variable to calculate the square root.
+ *
+ * Return: The square root of @n if it's a perfect square , otherwise -1.
+ */
+int sqrt(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	else if (guess * guess < n)
+	{
+		return (sqrt(n, guess + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
+/**
+ * _sqrt_recursion - Checks if the number is equal to zero or a negative number
+ *
+ * @n: The number whose square root will be calculated.
+ *
+ * Return: The square root of the number , if it doesn't have one it returns -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (sqrt(n, 1));
+	}
+}
