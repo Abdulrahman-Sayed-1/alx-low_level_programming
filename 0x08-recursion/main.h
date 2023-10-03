0de3627e9bc493071a4379da5b27961e0c3dbb80
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-03 15:18:23 -0700

    1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int is_prime(int n, int c);
+int is_plndrm(int len, int idx);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square_root(int n, int guess);
