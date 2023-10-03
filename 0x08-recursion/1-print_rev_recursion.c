77bf865b0b5532e4e9bedcec907dc4b3c8805243
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-03 12:03:07 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,36 @@
+/**
+ * _print_rev_recursion - Prints the string reversed.
+ *
+ * @s: The string to be printed.
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	static int end = 0;
+
+	if (end == 1)
+	{
+		putchar(*s);
+
+		if (*s == '\n')
+		{
+			return;
+		}
+
+		_print_rev_recursion(--s);
+	}
+	else
+	{
+		if (*s != '\0')
+		{
+			_print_rev_recursion(++s);
+		}
+		else
+		{
+			end = 1;
+
+			_print_rev_recursion(--s);
+		}
+	}
+}
