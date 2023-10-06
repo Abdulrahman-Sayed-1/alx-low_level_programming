0904b852c97a9acd9ecde641150466d86acc1ddc
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-06 13:49:45 -0700

    1

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - checks is a character is uppercase or not.
+ *
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is uppercase, otherwise 0.
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - checks if a character is lowercase.
+ *
+ * @c: the character to be checked.
+ *
+ * Return: 1 if c is lowercase, othrwise 0.
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
