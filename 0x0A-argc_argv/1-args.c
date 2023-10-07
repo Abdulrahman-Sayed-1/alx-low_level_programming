f8430a882037e13cbfc8136dfc99570dadb6ee9f
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-07 15:03:39 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Prints the number of its arguments.
+ *
+ * @argc: The number of command line arguments.
+ * @argv: The arguments of the commmand line.
+ *
+ * Return: on success 0.
+ */
+int main(int argc, char *argv[])
+{
+	int num;
+
+	num = argc - 1;
+
+	printf("%d\n", num);
+
+	return (0);
+}
+
