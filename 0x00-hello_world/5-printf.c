26329b72f3d5e9c9a4887301ff50ab3564628107
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-07 21:37:52 -0700

    1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return: 0
+ * main - prints a string using the printf function.
+ * Return: on success 0.
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
