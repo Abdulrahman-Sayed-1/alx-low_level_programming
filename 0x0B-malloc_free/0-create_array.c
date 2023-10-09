a883b9b29a888dab14a8b5297378bcbc67822118
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-09 15:36:56 -0700

    1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 
-	char *p = malloc(size);
+	char *p = malloc(size), *r;
 
 	if (size == 0)
 		return (NULL);
@@ -19,11 +19,13 @@ char *create_array(unsigned int size, char c)
 	if (p == NULL)
 		return (NULL);
 
+	r = p;
+
 	for (i = 0; i < size; ++i, ++p)
 	{
 		*p = c;
 	}
 
-	return (p - (size - 1));
+	return (r);
 }
 
