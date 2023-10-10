e78a8784c5f16253f1b53589697fd59d0df6f51e
Abdulrahman-Sayed-1 <fffds1456@gmail.com>
2023-10-09 17:10:32 -0700

    1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,12 @@ char *str_concat(char *s1, char *s2)
 	if ((*s1 == '\0' && *s2 == '\0') || (s1 == NULL && s2 == NULL))
 		return (NULL);
 
+	if (s1 == NULL)
+		*s1 = '\0';
+
+	if (s2 == NULL)
+		*s2 = '\0';
+
 	while (*s1 != '\0')
 	{
 		++size, ++s1;
